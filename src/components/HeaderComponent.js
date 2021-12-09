4f0fa05c7d4854f3529b6738dee2c4d288704337
Nageswara Rao <nageshraoj@gmail.com>
2021-12-09 18:04:37 +0530

    added few styles

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,18 +1,50 @@
 import { AppBar, Toolbar, Tabs, Tab, Container } from '@mui/material'
+import { createStyles } from '@mui/styles'
 import { Link } from 'react-router-dom'
 const HeaderComponent = () => {
+  const useStyle = createStyles({
+    tabStyle: {
+      borderRadius: '10px',
+      '&:hover': {
+        backgroundColor: '$F0BB62',
+      },
+    },
+  })
+
+  const style = useStyle()
+
   return (
     <>
       <Container>
         <AppBar>
           <Toolbar>
             <Tabs>
-              <Tab label='Home' to='/' component={Link} />
-              <Tab label='About' to='/About' component={Link} />
-              <Tab label='Contact' to='/Contact' component={Link} />
+              <Tab
+                className={style.tabStyle}
+                label='Home'
+                to='/'
+                component={Link}
+              />
+              <Tab
+                className={style.tabStyle}
+                label='About'
+                to='/About'
+                component={Link}
+              />
+              <Tab
+                className={style.tabStyle}
+                label='Contact'
+                to='/Contact'
+                component={Link}
+              />
             </Tabs>
             <Tabs sx={{ marginLeft: 'auto' }}>
-              <Tab label='Login' to='/Login' component={Link} />
+              <Tab
+                className={style.tabStyle}
+                label='Login'
+                to='/Login'
+                component={Link}
+              />
             </Tabs>
           </Toolbar>
         </AppBar>
