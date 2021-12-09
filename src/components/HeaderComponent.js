3e94bc06fbd47c43b3249816918c700efb1e26a5
Nageswara Rao <nageshraoj@gmail.com>
2021-12-09 17:08:48 +0530

    Updated scripts

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,22 @@
+import { AppBar, Toolbar, Tabs, Tab, Container } from '@mui/material'
+import { Link } from 'react-router-dom'
+const HeaderComponent = () => {
+  return (
+    <>
+      <Container>
+        <AppBar>
+          <Toolbar>
+            <Tabs>
+              <Tab label='Home' to='/' component={Link} />
+              <Tab label='About' to='/About' component={Link} />
+              <Tab label='Contact' to='/Contact' component={Link} />
+            </Tabs>
+          </Toolbar>
+        </AppBar>
+        <div style={{ marginBottom: '80px' }} />
+      </Container>
+    </>
+  )
+}
+
+export default HeaderComponent
